3aceac2916828131055074266236382b7d22fdb0
Javier Zarate <javalzar17@gmail.com>
2020-10-27 14:28:17 -0700

    isEven complete();

--- src/recursion.js
+++ src/recursion.js
@@ -58,6 +58,14 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  n = Math.abs(n);
+  if (n === 1) {
+    return false;
+  }
+  if (n === 2) {
+    return true;
+  }
+  return isEven(n - 2);
 };
 
 // 5. Sum all integers below a given integer.
