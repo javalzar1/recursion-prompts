a744f267a027bc2f9528508b59eb1d22148bba18
Javier Zarate <javalzar17@gmail.com>
2020-10-27 18:19:09 -0700

    Completed Palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -134,6 +134,21 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  if (string.length === 1) {
+    return true;
+  }
+  var last = string.length - 1;
+  var endsMatch = string.charAt(0).toLowerCase() === string.charAt(last).toLowerCase();
+  if (!endsMatch) {
+    return false;
+  }
+  if (string.length === 2 && endsMatch) {
+    return true;
+  }
+  if (endsMatch && string.length >= 3) {
+    return palindrome(string.slice(1, last));
+  }
+
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
